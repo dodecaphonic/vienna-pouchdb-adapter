44a800b4e8d554a21416583fee3f9154d064050b
Vitor Capela <dodecaphonic@gmail.com>
2015-04-02 14:47:25 -0300

    Implements #find.

--- opal/vienna/pouchdb_adapter.rb
+++ opal/vienna/pouchdb_adapter.rb
@@ -3,6 +3,11 @@ require "pouchdb"
 
 module Vienna
   class PouchDBAdapter < Adapter
+    SPECIAL_ATTRIBUTES = [:_id, :_rev, :rbtype]
+    ERROR_MESSAGES     = {
+      wrong_type: "Wrong type for %s: expected %s, received: %s"
+    }
+
     class << self
       def configure
         @configuration = Configuration.new
@@ -16,15 +21,23 @@ module Vienna
       def database
         fail "configure Adapter before usage" if !configuration
 
-        @database ||= PouchDB::Database.new(configuration)
+        @database ||= PouchDB::Database.new(name: configuration.database_name)
       end
     end
 
     def find(model, id, &block)
-      puts "I'm here"
       database.get(id).then do |attributes|
-        puts "I'm there"
-        block.call(model.load(attributes))
+        expected_type = model.class.to_s
+        model_type    = attributes[:rbtype]
+
+        if model_type != expected_type
+          error_message = format_error(:wrong_type, id, model_type, expected_type)
+          model.class.trigger :pouchdb_error, error_message
+        else
+          block.call(update_model(model, attributes))
+        end
+      end.fail do |error|
+        model.trigger :pouchdb_error, error.message
       end
     end
 
@@ -33,6 +46,22 @@ module Vienna
     def database
       self.class.database
     end
+
+    def update_model(model, attributes)
+      actual_attrs  = attributes.reject { |k, _| SPECIAL_ATTRIBUTES.member?(k) }
+      special_attrs = attributes.select { |k, _| SPECIAL_ATTRIBUTES.member?(k) }
+      final_attrs   = actual_attrs.merge(_vienna_pouchdb: special_attrs,
+                                         id: special_attrs[:_id])
+
+      model.load(final_attrs)
+
+      model
+    end
+
+    def format_error(error_type, *args)
+      format(ERROR_MESSAGES[error_type], *args)
+    end
+
 
     Configuration = Struct.new(:database_name)
   end
--- spec/adapter_spec.rb
+++ spec/adapter_spec.rb
@@ -20,8 +20,34 @@ describe Vienna::PouchDBAdapter do
   let(:db) { Vienna::PouchDBAdapter.database }
 
   describe "#find" do
-    async "updates a model" do
-      db.put(_id: "widget-1", name: "Golden Cog", part_number: 1337)
+    let(:raw_doc) {
+      { _id: "widget-1", name: "Golden Cog", part_number: 1337, rbtype: "Widget" }
+    }
+
+    it "has this test because it needs to exist for async to run don't know why"
+
+    async "fills in a Model's data" do
+      db.put(raw_doc).then do
+        Widget.find("widget-1") do |w|
+          async do
+            expect(w.name).to eq("Golden Cog")
+            expect(w.id).to eq("widget-1")
+            expect(w.part_number).to eq(1337)
+          end
+        end
+      end
+    end
+
+    async "triggers errors if stored type doesn't match the Model's" do
+      db.put(raw_doc.merge(rbtype: "OtherType")).then do
+        Widget.on :pouchdb_error do |error|
+          async do
+            expect(error).to match(/wrong type/i)
+          end
+        end
+
+        Widget.find("widget-1")
+      end
     end
   end
 end
